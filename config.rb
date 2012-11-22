2639aa835e2a42fd1609af1e512d2a4a84c50664
Drew Barontini <drewbarontini@gmail.com>
2012-11-22 13:52:35 -0500

    Compress CSS output file

--- config.rb
+++ config.rb
@@ -8,7 +8,7 @@ images_dir = "assets/images"
 javascripts_dir = "assets/javascripts"
 fonts_dir = "assets/fonts"
 
-output_style = :nested
+output_style = :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
