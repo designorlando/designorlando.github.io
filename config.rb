7f51173230227b4f9686be7276bbbef076e2dbc7
Drew Barontini <drewbarontini@gmail.com>
2012-11-22 13:25:48 -0500

    Initial design commit

---
+++ config.rb
@@ -0,0 +1,20 @@
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/stylesheets"
+images_dir = "assets/images"
+javascripts_dir = "assets/javascripts"
+fonts_dir = "assets/fonts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+color_output = false
+
+preferred_syntax = :sass
